4aba88a0b4309e68313bb7e76d9612ce86b71482
Jeff Micklos <jeffmicklos@gmail.com>
2013-10-23 12:39:52 -0700

    got static asset minification

--- april.py
+++ april.py
@@ -1,8 +1,10 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.assets import Environment, Bundle
 
 app = Flask(__name__)
+assets = Environment(app)
 
 @app.route('/')
 def hello():
-    return 'Hello April!'
+	return render_template('base.html')
