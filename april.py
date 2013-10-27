fcb7615eb7389e6eeb32396049a52bbab73620c6
Jeff Micklos <jeffmicklos@gmail.com>
2013-10-27 15:38:40 -0700

    mobile!

--- april.py
+++ april.py
@@ -1,10 +1,17 @@
 import os
-from flask import Flask, render_template
+import re
+from flask import Flask, render_template, request, g
 from flask.ext.assets import Environment, Bundle
+from  werkzeug.debug import get_current_traceback
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 assets = Environment(app)
 
 @app.route('/')
 def hello():
-	return render_template('base.html')
+
+  MOBILE_REGEX = 'palm|blackberry|nokia|phone|mobi|symbian|ericsson|motorola|samsung|portable|sprint|android|vodafone|ipod|webos|pocket|iphone|mobileexplorer'
+  g.is_mobile = bool(re.search(MOBILE_REGEX, request.user_agent.string, re.IGNORECASE))
+  
+  return render_template('base.html')
