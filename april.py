92a00c289aa5666c49ebd2b5863d428dc71ff904
Jeff Micklos <jeffmicklos@gmail.com>
2014-03-21 12:15:08 -0700

    remove libxml

--- april.py
+++ april.py
@@ -11,10 +11,8 @@ assets = Environment(app)
 
 @app.route('/')
 def hello():
-
   MOBILE_REGEX = 'palm|blackberry|nokia|phone|mobi|symbian|ericsson|motorola|samsung|portable|sprint|android|vodafone|ipod|webos|pocket|iphone|mobileexplorer'
   g.is_mobile = bool(re.search(MOBILE_REGEX, request.user_agent.string, re.IGNORECASE))
-  
   return render_template('base.html')
 
 @app.route('/vine')
@@ -40,7 +38,6 @@ def pinterest():
 def boards(username):
 	r = requests.get('http://pinterestapi.co.uk/'+username+'/boards')
 	return json.dumps(r.content)
-	#return json.dumps("{\"body\":[{\"name\":\"apartment\\n                            Jeff Micklos\",\"href\":\"\\/jeffmicklos\\/apartment\\/\",\"src\":\"http:\\/\\/media-cache-ak0.pinimg.com\\/216x146\\/79\\/ac\\/13\\/79ac13f5a88a7d7c3c19082effe45427.jpg\"},{\"name\":\"photography\\n                            Jeff Micklos\",\"href\":\"\\/jeffmicklos\\/photography\\/\",\"src\":\"http:\\/\\/media-cache-ak0.pinimg.com\\/custom_covers\\/216x146\\/34973403295349776_1361488285.jpg\"},{\"name\":\"mens wear \\u2013 dressing down\\n                            Jeff Micklos\",\"href\":\"\\/jeffmicklos\\/mens-wear-dressing-down\\/\",\"src\":\"http:\\/\\/media-cache-ak0.pinimg.com\\/custom_covers\\/216x146\\/34973403295296280_1345615783.jpg\"},{\"name\":\"womens wear \\u2013 dessing down\\n                            Jeff Micklos\",\"href\":\"\\/jeffmicklos\\/womens-wear-dessing-down\\/\",\"src\":\"http:\\/\\/media-cache-ec0.pinimg.com\\/custom_covers\\/216x146\\/34973403295296290_1361488441.jpg\"},{\"name\":\"Collaborative Board\\n                                            \\n                            \\n\\n\\n\\n\\n\\n\\n\\n\\n                    \\n                        \\n\\n                                \\n                \\n                                                \\n                    \\n        \\n                                    Everlane Office 2.0\\n                            Jeff Micklos\",\"href\":\"\\/jeffmicklos\\/everlane-office-20\\/\",\"src\":\"http:\\/\\/media-cache-ec0.pinimg.com\\/custom_covers\\/216x146\\/34973403295360721_1370457901.jpg\"},{\"name\":\"womens wear\\n                            Jeff Micklos\",\"href\":\"\\/jeffmicklos\\/womens-wear\\/\",\"src\":\"http:\\/\\/media-cache-ec0.pinimg.com\\/custom_covers\\/216x146\\/34973403295295543_1361488353.jpg\"},{\"name\":\"mens wear\\n                            Jeff Micklos\",\"href\":\"\\/jeffmicklos\\/mens-wear\\/\",\"src\":\"http:\\/\\/media-cache-ec0.pinimg.com\\/custom_covers\\/216x146\\/34973403295295542_1361488382.jpg\"},{\"name\":\"I am going to buy art\\n                            Jeff Micklos\",\"href\":\"\\/jeffmicklos\\/i-am-going-to-buy-art\\/\",\"src\":\"http:\\/\\/media-cache-cd0.pinimg.com\\/216x146\\/ae\\/13\\/3f\\/ae133f3b5e278c355aedfa37c6ba4350.jpg\"}],\"meta\":{\"count\":8}}")
 
 @app.route('/pinterest/<username>/<board>')
 def pins(username, board):
